899d9be9514495b768836da6494d58c31ee6fec7
PaulMarchal3615 <paulmarchal3615@gmail.com>
2022-01-28 18:09:47 +0100

    commit change BDD
    change on BDD structure
    --> analysis instead of samples in order to make the code run faster
    (NOT WORKING YET)

--- code/scripts/JodelMain.js
+++ code/scripts/JodelMain.js
@@ -8,7 +8,7 @@ document.getElementById('exportSamples').addEventListener("click",exportSamples)
 var db_jodel = new Dexie("jodelDB");
 
 db_jodel.version(1).stores({
-	samples:`NAME,FILE_NAME,COLOR,DISPLAY_TYPE,
+	analysis:`FILE_NAME,COLOR,DISPLAY_TYPE,
 	*A0,*A1,*A2,*A3,*A4,*A5,*A6,*A7,*A8,*A9,*A10,*A11,*A12,*A13,*A14,*A15,*A16,*A17,*A18,*A19,*A20,*A21,*A22,*A23,*A24,*A25,*A26,*A27,*A28,*A29,*A30,*A31,*A32,*A33,*A34,*A35,*A36,*A37,*A38,*A39,*A40,**A41,*A42,*A43,*A44,*A45,*A46,*A47,*A48,*A49,*A50,*A51,*A52,*A53,*A54,*A55,*A56,*A57,*A58,*A59,*A60,*A61,*A62,*A63,*A64,*A65,*A66,*A67,*A68,*A69,*A70,*A71,*A72,*A73,*A74,*A75,*A76,*A77,*A78,
 	*B0,*B1,*B2,*B3,*B4,*B5,*B6,*B7,*B8,*B9,*B10,*B11,*B12,*B13,*B14,*B15,*B16,*B17,*B18,*B19,*B20,*B21,*B22,*B23,*B24,*B25,*B26,*B27,*B28,*B29,*B30,*B31,*B32,*B33,*B34,*B35,*B36,*B37,*B38,*B39,*B40,*B41,*B42,*B43,*B44,*B45,*B46,*B47,*B48,*B49,*B50,*B51,*B52,*B53,*B54,*B55,*B56,*B57,*B58,*B59,*B60,*B61,*B62,*B63,*B64,*B65,*B66,*B67,*B68,*B69,
 	*C0,*C1,*C2,*C3,*C4,*C5,*C6,*C7,*C8,*C9,*C10,*C11,*C12,*C13,*C14,*C15,*C16,*C17,
@@ -21,21 +21,21 @@ db_jodel.version(1).stores({
 	*H0,*H1,*H2,*H3,*H4,*H5,*H6,*H7,*H8,*H9,*H10,*H11,*H12,*H13,*H14,*H15,
 	*K0,*K1,*K2,*K3,*K4,*K5,*K6,*K7,*K8,*K9,*K10,*K11,*K12,*K13,*K14,*K15,*K16,*K17,*K18,*K19,*K20,*K21,*K22,*K23,*K24,*K25,*K26,*K27,*K28,*K29,*K30,*K31,*K32,*K33,*K34,
 	*L0,*L1,*L2,*L3,*L4,*L5,*L6,*L7,*L8,*L9,*L10,*L11,*L12,*L13,*L14,*L15,*L16,*L17,*L18,*L19,*L20,*L21,*L22,*L23,*L24,*L25,*L26,*L27,*L28,*L29,*L30,*L31,*L32,*L33,*L34,*L35,*L36,*L37,*L38,*L39,*L40,*L41,*L42,*L43,*L44,*L45,*L46,*L47`, 
-	datasets:`FILE_NAME,ARRAY,TYPE,COLOR`,
 	holes:`HOLEID,HOLEID_LATITUDE,HOLEID_LONGITUDE,COLOR,FILE_NAME`,
-	var:`FILE_NAME,VARLIST`
+	datasets:`FILE_NAME,A0,ARRAY,COLOR,ANALYSIS`
 });
 
 db_jodel.open().catch(function (e) {
-    console.error("Open failed: " + e);
+	console.error("Open failed: " + e);
 })
 
 // clear stores if reload page 
 
-db_jodel.samples.clear();
+db_jodel.analysis.clear();
 db_jodel.holes.clear();
 db_jodel.datasets.clear();
-db_jodel.var.clear();
+
+
 
 // add event listener on file input
 var input = document.querySelector('#fileInput');
@@ -101,20 +101,19 @@ subfilter.addEventListener('change', filteredDisplay);
 
 function parseFiles(event) {
 
-	var a = performance.now();
 	var id = event.target.id;
 
 	if (id == "fileInput") {
 
 		for (var file of input.files) {
+			console.log(file);
 			if (file.name.split('.').pop() =="csv") {
 
 				Papa.parse(file, {
 					download: true,
 					complete: function(results) {
 
-						db_jodel.transaction('rw', db_jodel.datasets,db_jodel.holes,db_jodel.samples,db_jodel.var, function () {
-
+						db_jodel.transaction('rw', db_jodel.datasets,db_jodel.holes, function () {
 
 							db_jodel.datasets.add({
 								FILE_NAME: file.name,
@@ -126,12 +125,10 @@ function parseFiles(event) {
 							db_jodel.datasets.where("FILE_NAME").equals(file.name)
 							.each(function (dataset) {
 								updateFileTable(dataset);
-								buildSamplesBase(dataset);
+								readDataset(dataset);
 						
 							}).then(function() {
-								displayMain();
-								var z = performance.now();
-								console.log('Total,' + (z - a));
+								//displayMain();
 							});	
 						})		
 					}
@@ -141,110 +138,31 @@ function parseFiles(event) {
 	}	
 }
 
+function readDataset(dataset) {
 
-function jsonConcat(o1, o2) {
-	for (var key in o2) {
-		var c = o1[key].concat(o2[key]);
-		var d = c.filter(function (item, pos) {return c.indexOf(item) == pos});
-		o1[key] = d;
-	}
-	return o1;
-}
+	const headersKeys = dataset.ARRAY.map(getKeyByValue);
 
+	const fn = ([keys, ...values]) => 
+	values.map(vs => Object.fromEntries(vs.map((v, i) => [headersKeys[i], v])));
+	const result = fn(dataset.ARRAY);
+	const colors = result[A0].map(x => dataset.COLOR);
+	result["COLOR"] = colors;
 
-function buildSamplesBase(dataset) {
+	const fileName = result[A0].map(x => dataset.FILE_NAME);
+	result["FILE_NAME"] = fileName;
 
-	var b = performance.now();
+	const type = result[A0].map(x => dataset.TYPE);
+	result["TYPE"] = type;
 
-	var headers, units, lines;
-	[headers, units, ...lines] = dataset.ARRAY;
-
-	const headersKeys = headers.map(getKeyByValue); // for simple keys A0,... to avoid use of variable name with dashes
-
-	var allVar = {};
-	headersKeys.forEach((key, i) => allVar[key] = []);
-
-	var c = performance.now();
-	console.log('1,' + (c - b));
-	
-
-	const samplesColIndice = headers.findIndex(element =>element === "SAMPLING_POINT-NAME");
-	const allSamples = lines.map(function(value,index) { return value[samplesColIndice]; });
-	const uniqueSamples = allSamples.filter((v, i, a) => (a.indexOf(v) === i)).filter(n=>n); //keep unique values and only values != "" or undefined
-
-	var d = performance.now();
-	console.log('2,' + (d - c));
-
-	
-	for (let sampleName of uniqueSamples) {
-
-		var miniSample = {};
-		miniSample.NAME = sampleName;
-		miniSample.FILE_NAME = dataset['FILE_NAME'];
-		miniSample.DISPLAY_TYPE = dataset['TYPE'];
-		miniSample.COLOR = dataset['COLOR'];
-
-		const lines = filter2DArray(dataset.ARRAY, samplesColIndice, sampleName);
-		const JSON = compactLines(lines, headersKeys);
-		const sample = Object.assign({},miniSample,JSON);
-		allVar = jsonConcat(allVar,JSON);
-
-		createHole(sample);
-
-		db_jodel.samples.add(sample)
-		.catch((error => {
-			alert("ERROR : createSample",error);
-		}));
-
-	}
-
-	var e = performance.now();
-	console.log('3,' + (e - d));
-
-	db_jodel.var.add({FILE_NAME:dataset.FILE_NAME,VARLIST:allVar})
+	db_jodel.analysis.bulkPut(result)
 	.catch((error => {
-		alert("ERROR : createvarlist",error);
+		alert("ERROR : Init_BDD_analysis",error);
 	}));
 
-	var f = performance.now();
-	console.log('4,' + (f - e));
-
-
 }
 
-/**
- * 
- * @param {*} array 2D array to filter
- * @param {*} colNumber column to 
- * @param {*} VOI : value of interest
- * @returns lines where array[col] === VOI
- */
-function filter2DArray(array, colNumber, VOI) {
-	return array.filter(line=> line[colNumber]===VOI);
-}
 
-/**
- * 
- * @param {*} lines array of lines concerning a sample in initial array
- * @param {*} headersKeys array of heads codes (cf ressources.js --> fields)
- * @returns an Object JSON with for each property code, an associated list of all encountered values for this sample.
- */
-function compactLines(lines, headersKeys) {
-	var JSON ={};
 
-	for (var i =0; i<headersKeys.length; i++) {
-		JSON[headersKeys[i]] =[];
-		for (var j=0; j<lines.length; j++) 
-		{
-			const val = parseFloat(lines[j][i]) || lines[j][i];
-			if (!JSON[headersKeys[i]].includes(val) || isFloat(val)) {
-				
-				JSON[headersKeys[i]].push(val);
-			}
-		}
-	}
-	return JSON;
-}
 
 /**
  * 
