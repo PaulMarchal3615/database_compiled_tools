9567f2caae89c025896b9f9326f5bdac5cac310c
Paul Marchal <marchal79@univ-lorraine.fr>
2022-03-30 19:39:00 +0200

    COMMIT
    COMMIT

--- code/scripts/BDDCreator/BDDExport.js
+++ code/scripts/BDDCreator/BDDExport.js
@@ -1,3 +1,6 @@
+import {getKeyByValue} from "../Common/common_functions.js";
+import {units, keyVal, abbrev} from "../Common/ressources.js";
+
 //---------------------------------------------
 // 1. init dexie db : db_BDD with two stores : analysis (based on analysis lines of a file and datasets to store files info)
 
@@ -25,15 +28,111 @@ db_BDD.rawMetadata_files.clear();
 
 export function exportData() {
 
-    console.log('protiprouta');
+
+        db_BDD.transaction('r', db_BDD.analysis_files, function() {
+            return db_BDD.analysis_files.toArray();
+        }).then(analysis_files => {
+
+            var types = getTypeList(analysis_files);
+            console.log(types);
+
+            for (var type of types) {
+
+                console.log(analysis_files.filter(file => file.TYPE == type));
+
+                var filteredfiles = analysis_files.filter(file => file.TYPE == type);
+                var array = convertToArray(filteredfiles);
+                console.log(array);
+            }
+            
+        })
+        .catch (function (e) {
+            console.error("Load ANALYSIS FILES ERROR : ",e);
+        });	
 
     return 0;
 
 }
 
-export function showData() {
+function convertToArray(files) {
 
-    console.log('prout');
+    var array =[];
+    var headers, unitsLine;
+    [headers, unitsLine] = getHeadersLine(files);
+    array.push(headers, unitsLine);
+    headers.unshift('ANALYSIS_ABBREV');
+    headers.unshift('MEASUREMENT-ABBREV');
+    headers.unshift('MEASUREMENT-NATURE');
+    unitsLine.unshift('text');
+    unitsLine.unshift('text');
+    unitsLine.unshift('text');
+
+    for (var file of files) {
+
+        for (var i = 0; i <file.CORRECT_DICT[Object.keys(file.CORRECT_DICT)[0]].length; i++) {
+
+            var line = [];
+
+            for (var head of headers) {
+
+                if ((head == 'ANALYSIS_ABBREV') || (head == 'MEASUREMENT-NATURE')) {
+                    line.push(abbrev[file.TYPE]);
+                }
+
+                else if (head == 'MEASUREMENT-ABBREV') {
+                    line.push(file.TYPE);
+                }
+                else {
+                    var values = file.CORRECT_DICT[head];
+                    line.push(values[i]);   
+                }
+
+
+            }
+            array.push(line);
+        }
+    }
+    return array;
+
+}
+
+function getTypeList(files) {
+    var types =[];
+
+    for (var file of files) {
+
+        types.push(file.TYPE);
+
+    }
+
+    return types.filter((v, i, a) => a.indexOf(v) === i);
+}
+
+function getHeadersLine(analysis_files) {
+    var headers = [];
+    var unitsList = [];
+
+    for (var file of analysis_files) {
+        headers = headers.concat(Object.keys(file.CORRECT_DICT));
+    }
+    var unique = headers.filter((v, i, a) => a.indexOf(v) === i);
+
+    for ( var value of unique) {
+        var key = getKeyByValue(keyVal, value);
+        var unit = units[key];
+        unitsList.push(unit);
+    }
+
+    return [unique, unitsList];
+}
+
+function mergeAnalysis(analysis_files) {
+
+    return Array;
+
+}
+
+export function showData() {
 
     db_BDD.transaction('r', db_BDD.rawMetadata_files, function() {
         return db_BDD.rawMetadata_files.toArray();
@@ -62,11 +161,8 @@ export function showData() {
 
 function updateExportTable(metadataFiles, analysisFiles) {
 
-    console.log(metadataFiles, analysisFiles);
-
-
     var tablebody = document.getElementById("ExportTable").getElementsByTagName('tbody')[0];
-    $("#ExportTable tr").remove();
+    $("#ExportTable tbody tr").remove();
     
     for (var file of metadataFiles) {
         var row = tablebody.insertRow(0);
